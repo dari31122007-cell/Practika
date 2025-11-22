f79a99c4947c7a3e2b5181ef74ced4a1883619a2
Кирюшина Дарина Евгеньевна <10230910@college.local>
2025-11-22 13:50:37 +0300

    1 version

---
+++ Practika/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Practika
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
